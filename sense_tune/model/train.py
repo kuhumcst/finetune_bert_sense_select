19f0ee02d8dd69fcb892e4220b314cb89b023d38
nmp828 <BrownieGIT123>
2021-12-14 00:44:58 +0100

    minor edit

--- sense_tune/model/train.py
+++ sense_tune/model/train.py
@@ -6,7 +6,7 @@ from sense_tune.model.bert import BertSense, forward
 
 
 def train(model, train_dataloader, device, learning_rate=1e-4,
-          num_epochs=5, max_steps=0, max_grad_norm=1.0):
+          num_epochs=5, max_steps=10, max_grad_norm=1.0):
     """ Fine-tune the model """
     if max_steps > 0:
         max_steps
@@ -125,6 +125,9 @@ def evaluate(model, eval_dataloader, device):
         eval_loss += loss
         prediction = logits_list[0].topk(1).indices
         predictions.extend(prediction)
+
+        print(prediction)
+        print(labels)
 
         if 1 in labels[prediction]:
             accuracy += 1
