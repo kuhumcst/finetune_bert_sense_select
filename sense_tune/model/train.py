0b1597daecb5c9f03cc4277c9e551e01b03da4ea
nmp828 <BrownieGIT123>
2022-01-11 22:07:36 +0100

    edit back

--- sense_tune/model/train.py
+++ sense_tune/model/train.py
@@ -46,7 +46,6 @@ def train(model, train_dataloader, device, learning_rate=1e-4,
                     logits = forward(model, batch[2:], device)
 
                     targets = torch.max(batch[5].to(device), -1).indices.to(device).detach()
-                    #batch_loss += loss_function(logits, targets)#, batch[3].to(device).detach())
                     batch_loss += loss_function(logits.unsqueeze(dim=0), targets.unsqueeze(dim=-1))
 
                     logits = model.sigmoid(logits)
