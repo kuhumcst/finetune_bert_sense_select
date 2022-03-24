6895d1b4ccc6316c4ddf77fc727af1df34d67122
NatCHS <55261832+Linguistcoder@users.noreply.github.com>
2022-03-24 09:55:40 +0100

    Update reduce.py

--- sense_tune/model/reduce.py
+++ sense_tune/model/reduce.py
@@ -37,7 +37,7 @@ def get_BERT_score(data):
             # run model
             for batch in batches:
                 logits = forward(model, batch[2:], device)
-                #logits = model.sigmoid(logits)
+                logits = model.sigmoid(logits)
                 score.append(logits)
 
         nb_eval_steps += 1
